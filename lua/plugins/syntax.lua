5b2f01dc46aeb943a9df2b49d2672196b3762599
qwqFranzFox <qwqfranzfox@outlook.com>
2026-02-28 14:58:53 +0800

    deleted unused files and optimized configurations

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,40 +1,24 @@
 return {
-
+  -- Colorizer
   {
     "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup({
-        "*", -- Highlight all files, but customize some others.
-        css = { rgb_fn = true }, -- Enable parsing rgb(...) functions in css.
-        html = { names = false }, -- Disable parsing "names" like Blue or Gray
-      })
-    end,
+    opts = {
+      "*",                      -- Highlight all files, but customize some others.
+      css = { rgb_fn = true },  -- Enable parsing rgb(...) functions in css.
+      html = { names = false }, -- Disable parsing "names" like Blue or Gray
+    }
   },
 
   -- Shortcuts linting
-  {
-    "folke/which-key.nvim",
-    config = function() end,
-  },
+  { "folke/which-key.nvim", },
 
   -- Auto Pairs
-  {
-    "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup({})
-    end,
-  },
+  { "windwp/nvim-autopairs", },
 
   -- Todo Highlight
   {
     "folke/todo-comments.nvim",
     dependencies = "nvim-lua/plenary.nvim",
-    config = function()
-      require("todo-comments").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      })
-    end,
+    opts = {}
   },
 }
